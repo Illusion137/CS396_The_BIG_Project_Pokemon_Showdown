45f3a2eb4c6804ebf5311d1fb7c6dc630ace5ae7
Illusion137 <raygoza.d@hotmail.com>
2026-07-23 17:42:10 -0700

    feat: finished pokemon validator I think + mainly validating IVS and hidden power

--- prolog_validator/validator.pl
+++ prolog_validator/validator.pl
@@ -1,6 +1,20 @@
 :- [gen_db].
+:- [hidden_power_db].
 :- use_module(library(json)).
 :- use_module(library(pairs)).
+
+hidden_power_types(['Fighting', 'Flying', 'Poison', 'Ground', 'Rock', 'Bug', 'Ghost', 'Steel', 'Fire', 'Water', 'Grass', 'Electric', 'Psychic', 'Ice', 'Dragon', 'Dark']).
+
+hidden_power_type_from_ivs(IVs, Type) :-
+    Bits is (IVs.hp mod 2) 
+        + 2 * (IVs.atk mod 2) 
+        + 4 * (IVs.def mod 2) 
+        + 8 * (IVs.spe mod 2) 
+        + 16 * (IVs.spa mod 2) 
+        + 32 * (IVs.spd mod 2),
+    Index is (Bits * 15) // 63,
+    hidden_power_types(Types),
+    nth0(Index, Types, Type).
 
 has_index(Array, Index) :-
     nth0(Index, Array, _).
@@ -13,13 +27,37 @@ is_unique([X|Xs]) :-
 pokemon_move_valid(Name, Move) :-
     pokemon(Name),
     move(Move),
-    has_move(Name, Move).
+    ( hidden_power(Move)
+        -> has_move(Name, 'Hidden Power')
+        ; has_move(Name, Move)
+    ).
 
 pokemon_all_moves_valid(Name, Moves) :-
     (\+ has_index(Moves, 0) ; nth0(0, Moves, M0), pokemon_move_valid(Name, M0)),
     (\+ has_index(Moves, 1) ; nth0(1, Moves, M1), pokemon_move_valid(Name, M1)),
     (\+ has_index(Moves, 2) ; nth0(2, Moves, M2), pokemon_move_valid(Name, M2)),
     (\+ has_index(Moves, 3) ; nth0(3, Moves, M3), pokemon_move_valid(Name, M3)).
+
+pokemon_hidden_power_valid(Pokemon) :-
+    include(hidden_power, Pokemon.moves, HiddenPowerMoves),
+    length(HiddenPowerMoves, Count),
+    Count =< 1,
+    (HiddenPowerMoves = [HiddenPowerMove]
+        -> hidden_power_type_from_ivs(Pokemon.ivs, Type),
+            format(atom(BracketedType), "Hidden Power [~w]", [Type]),
+            format(atom(PlainType), "Hidden Power ~w", [Type]),
+            ( HiddenPowerMove == BracketedType
+                ; HiddenPowerMove == PlainType
+            )
+        ; true
+    ).
+
+pokemon_nickname_valid(Pokemon) :-
+    ( get_dict(nickname, Pokemon, Nickname)
+    -> atom_length(Nickname, Len),
+       Len =< 10
+    ; true
+    ).
 
 pokemon_ability_valid(Name, Ability) :-
     pokemon(Name),
@@ -36,7 +74,9 @@ is_pokemon_valid(Pokemon) :-
     nature(Pokemon.nature),
     pokemon_ability_valid(Pokemon.name, Pokemon.ability),
     pokemon_all_moves_valid(Pokemon.name, Pokemon.moves),
-    pokemon_evs_valid(Pokemon.evs).
+    pokemon_evs_valid(Pokemon.evs),
+    pokemon_hidden_power_valid(Pokemon),
+    pokemon_nickname_valid(Pokemon).
 
 is_team_valid(PokemonTeam) :-
     maplist(is_pokemon_valid, PokemonTeam).
@@ -60,8 +100,6 @@ main(Argv) :-
     Argv = [PlayerJSONPath, OpponentJSONPath],
     read_pokemon_file(PlayerJSONPath, PlayerTeam),
     read_pokemon_file(OpponentJSONPath, OpponentTeam),
-    format("Team 1 (~w): ~w Pokemon~n", [PlayerJSONPath, PlayerTeam]),
-    format("Team 2 (~w): ~w Pokemon~n", [OpponentJSONPath, OpponentTeam]),
     maplist(print_pokemon, PlayerTeam),
     maplist(print_pokemon, OpponentTeam),
     is_team_valid(PlayerTeam),
