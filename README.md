9b17ec774e523958c5adc349f81c5f432c65dd9d
Illusion137 <raygoza.d@hotmail.com>
2026-07-23 23:09:36 -0700

    chore: update README + fix linux maybe

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 - Prolog
 - g++ or other c++ compiler
 - CMake
+- ninja (for CMake)
 - ts-node (not for use but if you want to expand generation; everything generated comes prebuilt)
 
 ## Usage Example (Individual)
@@ -23,7 +24,7 @@ swipl ./prolog_validator/validator.pl -- ./.tmp/player.json ./.tmp/opponent.json
 ```bash
 cmake -S cpp_player -B cpp_player/build -G Ninja
 cmake --build cpp_player/build
-./cpp_player/build/Showdown_Player.exe ./teams/player.showdown ./teams/opponent.showdown
+./cpp_player/build/Showdown_Player ./teams/player.showdown ./teams/opponent.showdown
 ```
 
 ## How to play
--- cpp_player/src/Utils.h
+++ cpp_player/src/Utils.h
@@ -19,10 +19,15 @@ void print(const std::string &fmt, Args&&... args) {
     std::cout << std::vformat(fmt, std::make_format_args(args...));
 }
 #elif
+// I thought I might need this to be different for linux so idek; potential useless #elif
 inline void println() { std::cout << '\n'; }
 template<typename... Args>
 void println(const std::string &fmt, Args&&... args) {
-    std::println(fmt, args);
+    std::cout << std::vformat(fmt, std::make_format_args(args...)) << '\n';
+}
+template<typename... Args>
+void print(const std::string &fmt, Args&&... args) {
+    std::cout << std::vformat(fmt, std::make_format_args(args...));
 }
 #endif
 
